16cd5722720d20825c137d0e7ae576b39b6b660e
Eugene <supernovaprotocol@gmail.com>
2024-10-17 17:42:05 +0700

    Create a utils module, move helper functions there

--- qa_tools/app/__init__.py
+++ qa_tools/app/__init__.py
@@ -8,6 +8,7 @@ from collections import namedtuple
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from app.render import render_indexjs, render_webpack_config
+from app.utils import clear_imports, parse_name, props_to_string, save_to_file, load_file
 
 app = FastAPI()
 
@@ -106,86 +107,6 @@ def fix_css_imports(js_code, css_entries):
     return js_code
 
 
-def clear_bracket_imports(imports_str, to_remove):
-    import_list = [name.strip() for name in imports_str.split(',')]
-    import_list = [name for name in import_list if name and name != to_remove]
-    return ', '.join(import_list) if import_list else ''
-
-
-def clear_default_and_named_import(regex, text, to_remove):
-    matches = re.findall(regex, text)
-    default_import, imports_str, lib_str = matches[0]
-    new_import_str = clear_bracket_imports(imports_str, to_remove)
-    default_import = "" if default_import == to_remove else default_import
-
-    if default_import and new_import_str:
-        return 'import {}, {{ {} }} from {}'.format(default_import, new_import_str.strip(), lib_str)
-
-    if default_import:
-        return 'import {} from {}'.format(default_import, lib_str)
-    
-    if new_import_str:
-        return 'import {{ {} }} from {}'.format(new_import_str, lib_str)
-    
-    return ""
-
-
-def clear_default_with_namespace_import(regex, text, to_remove):
-    matches = re.findall(regex, text)
-    default_import, name_space, lib_str = matches[0]
-    if default_import == to_remove:
-        return f'import * as {name_space} from {lib_str}'
-    return text
-
-
-def clear_named_import(regex, text, to_remove):
-    matches = re.findall(regex, text)
-    imports_str, lib_str = matches[0]
-    new_import_str = clear_bracket_imports(imports_str, to_remove)
-
-    if not new_import_str:
-        return ""
-
-    return 'import {{ {} }} from {}'.format(new_import_str, lib_str)
-
-
-def clear_default_import(regex, text, to_remove):
-    matches = re.findall(regex, text)
-    if matches and matches[0] == to_remove:
-        return ""
-    return text
-
-
-def clear_imports(code, to_remove="React"):
-
-    def fix_import_line(match):
-        text = match.group(0).strip()
-
-        default_and_named_import = 'import\s+([a-zA-Z]+)\s*,\s*{(.*)}\s+from\s+(.*)'
-
-        if re.search(default_and_named_import, text):
-            return clear_default_and_named_import(default_and_named_import, text, to_remove)
-
-        default_and_namespace_import = 'import\s+([a-zA-Z]+)\s*,\s*\* as ([a-zA-Z]+)\s+from\s+(.*)'
-
-        if re.search(default_and_namespace_import, text):
-            return clear_default_with_namespace_import(default_and_namespace_import, text, to_remove)
-
-        named_import = 'import\s+{(.*)}\s+from\s+(.*)'
-        
-        if re.search(named_import, text):
-            return clear_named_import(named_import, text, to_remove)
-
-        default_import = 'import\s+([a-zA-Z]+)\s+from'
-        if re.search(default_import, text):
-            return clear_default_import(default_import, text, to_remove)
-
-        return text
-
-    pattern = re.compile('\s*import .* from .*')
-    return pattern.sub(fix_import_line, code)
-
-
 class BadSourceCodeError(Exception):
     def detail(self):
         return "Bad format of source_tree"
@@ -209,15 +130,6 @@ class EmptyJavascriptFileError(BadSourceCodeError):
 class MalformedFileEntryError(BadSourceCodeError):
     def detail(self):
         return f'Malformed file entry: missing required field "{self.args[0]}"'
-
-
-def parse_name(code):
-    return 'MainComponent'
-
-
-def props_to_string(props: dict) -> str:
-    props = props or {}
-    return " ".join(f'{k}="{v}"' for k, v in props.items())
 
 
 class SimpleReactBuilder:
@@ -273,16 +185,6 @@ class SimpleReactBuilder:
     @property
     def output_dir(self):
         return os.path.join(self.build_directory, "artifacts")
-
-
-def save_to_file(path, content):
-    with open(path, "w") as f:
-        f.write(content)
-
-
-def load_file(path):
-    with open(path) as f:
-        return f.read()
 
 
 def build(src_tree, props=None):
