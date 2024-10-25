65769f31e3aa7d6cdfa0e1e586c2390d5d82b90f
Eugene <supernovaprotocol@gmail.com>
2024-10-25 12:05:45 +0700

    Adding some models

--- django/assistant/models.py
+++ django/assistant/models.py
@@ -43,3 +43,83 @@ class Configuration(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Monitor:
+    def __init__(self, suite, operations_reverse_relation):
+        self.suite = suite
+        self.operations_reverse_relation = operations_reverse_relation
+
+    @property
+    def running_operations(self):
+        relation = self.get_relation()
+        return relation.filter(finished=False)
+
+    @property
+    def crashed_operations(self):
+        relation = self.get_relation()
+        return relation.filter(finished=True, errors__isnull=False)
+
+    @property
+    def failed_operations(self):
+        relation = self.get_relation()
+        return relation.filter(finished=True, errors__isnull=True, success=False)
+
+    @property
+    def successful_operations(self):
+        relation = self.get_relation()
+        return relation.filter(finished=True, success=True)
+
+    @property
+    def all_finished(self):
+        return self.running_operations.count() == 0
+
+    @property
+    def first_successful(self):
+        return self.successful_operations.first()
+
+    def get_relation(self):
+        return getattr(self.suite, self.operations_reverse_relation)
+
+
+class Revision(models.Model):
+    src_tree = models.JSONField()
+
+
+class OperationSuite(models.Model):
+    revision = models.ForeignKey(Revision, related_name="suites", on_delete=models.CASCADE)
+
+    @property
+    def builds_monitor(self):
+        return Monitor(self, "builds")
+
+    @property
+    def lints_monitor(self):
+        return Monitor(self, "lints")
+
+    @property
+    def tests_monitor(self):
+        return Monitor(self, "tests")
+
+    @property
+    def interactions_monitor(self):
+        return Monitor(self, "interactions")
+
+    @property
+    def complete(self):
+        return (self.builds_monitor.all_finished and 
+                self.lints_monitor.all_finished and 
+                self.tests_monitor.all_finished and 
+                self.interactions_monitor.all_finished)
+
+
+class Operation(models.Model):
+    finished = models.BooleanField(default=False)
+    success = models.BooleanField(blank=True, null=True)
+    errors = models.JSONField(blank=True, null=True)
+    
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
