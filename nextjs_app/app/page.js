c0baeecf44efd9b9b40a150ca894c2c110bf9a9f
Eugene <supernovaprotocol@gmail.com>
2024-11-12 13:43:48 +0700

    Initial commit for nextjs frontend

---
+++ nextjs_app/app/page.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import "@/app/page.css"
+
+
+function Preset() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+
+export default function Home() {
+  return (
+    <div className="my-grid">
+      <div className="my-grid-item">Greeting!</div>
+    </div>
+  );
+}
