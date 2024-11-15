7cbc71b2823dca99fdff77f725efa56aed3e565a
Eugene <supernovaprotocol@gmail.com>
2024-11-15 22:10:13 +0700

    Fix modal container width

--- nextjs_app/app/components/modal.js
+++ nextjs_app/app/components/modal.js
@@ -15,27 +15,25 @@ const Modal = ({ show = false, onClose, children }) => {
           }}
         >
           {/* Modal Content */}
-          <div className="">
-            <div
-              className="bg-white rounded-lg shadow-lg mt-10 transition-all transform scale-95"
-              style={{
-                animation: show ? "slideDown 0.3s ease-out" : "slideUp 0.3s ease-in",
-              }}
-            >
-              <header className="bg-sky-800 text-white text-2xl pt-2 pb-2 pl-6 pr-6 rounded-t-md">
-                <button
-                  onClick={onClose}
-                  className="text-white hover:text-gray-200 float-right"
-                >
-                  <FontAwesomeIcon icon={faRectangleXmark} />
-                </button>
-                <h3>Add new server</h3>
-                {/* Close Button */}
+          <div
+            className="bg-white rounded-lg shadow-lg mt-10 transition-all transform scale-95 w-full md:w-1/2"
+            style={{
+              animation: show ? "slideDown 0.3s ease-out" : "slideUp 0.3s ease-in",
+            }}
+          >
+            <header className="bg-sky-800 text-white text-2xl pt-2 pb-2 pl-6 pr-6 rounded-t-md">
+              <button
+                onClick={onClose}
+                className="text-white hover:text-gray-200 float-right"
+              >
+                <FontAwesomeIcon icon={faRectangleXmark} />
+              </button>
+              <h3>Add new server</h3>
+              {/* Close Button */}
 
-              </header>
-              <div className="overflow-y-auto max-h-[80vh]">
-              {children}
-              </div>
+            </header>
+            <div className="overflow-y-auto max-h-[80vh]">
+            {children}
             </div>
           </div>
         </div>
