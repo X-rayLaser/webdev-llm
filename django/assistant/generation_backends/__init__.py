5b7ea7bb38f4f0569f5a42d7d349bae4093e5b28
Eugene <supernovaprotocol@gmail.com>
2026-01-22 19:26:07 +0700

    Refactor backend, new backend for responses, work in progress

--- django/assistant/generation_backends/__init__.py
+++ django/assistant/generation_backends/__init__.py
@@ -1,9 +1,10 @@
 import time
 import os
+import json
 import httpx
 import openai
 from openai import DefaultHttpxClient
-from .base import CompletionBackend, ChatCompletionJob
+from .base import CompletionBackend, ChatCompletionJob, ResponsesBackend
 from .adapters import CompletionBackendAdapter
 
 
@@ -30,34 +31,18 @@ class DummyCoderBackend(DummyBackend):
     separator = ""
 
 
-class OpenAICompatibleBackend(CompletionBackend):
-
-    def generate(self, job: ChatCompletionJob):
+class OpenaiHelperMixin:
+    def get_openai_client(self, url):
         http_client = self.get_http_client()
 
         timeout = 30 * 60 # 30 minutes
 
-        params = self.prepare_params(job)
-
-        client = openai.OpenAI(
-            base_url=f"{job.base_url}/v1",
+        return openai.OpenAI(
+            base_url=url,
             api_key="sk-no-key-required",
             timeout=timeout,
             http_client=http_client
         )
-
-        stream = client.chat.completions.create(
-            model=job.model,
-            messages=job.messages,
-            stream=True,
-            extra_body={"cache_prompt": True},
-            **params
-        )
-
-        for chunk in stream:
-            chunk_text = chunk.choices[0].delta.content or ""
-            self.response += chunk_text
-            yield chunk_text
 
     def get_http_client(self):
         http_proxy = os.environ.get("http_proxy_url")
@@ -83,6 +68,73 @@ class OpenAICompatibleBackend(CompletionBackend):
             "n_predict": "max_tokens"
         }
         return {mapping[name]:value for name, value in job.params.items() if name in mapping}
+
+
+class OpenAICompatibleBackend(OpenaiHelperMixin, CompletionBackend):
+
+    def generate(self, job: ChatCompletionJob):
+        params = self.prepare_params(job)
+
+        client = self.get_openai_client(f"{job.base_url}/v1")
+
+        stream = client.chat.completions.create(
+            model=job.model,
+            messages=job.messages,
+            stream=True,
+            extra_body={"cache_prompt": True},
+            **params
+        )
+
+        for chunk in stream:
+            chunk_text = chunk.choices[0].delta.content or ""
+            self.response += chunk_text
+            yield chunk_text
+
+
+class OpenAICompatibleResponsesBackend(OpenaiHelperMixin, ResponsesBackend):
+    def __init__(self) -> None:
+        super().__init__()
+        self.response = []
+
+    def generate(self, job: ChatCompletionJob):
+        client = self.get_openai_client(job.url)
+        params = self.prepare_params(job)
+
+        self.response = []
+
+        while True:
+            stream = client.responses.create(
+                model=job.model,
+                inputs=job.messages + self.response,
+                stream=True,
+                extra_body={"cache_prompt": True},
+                **params
+            )
+
+            got_func = False
+
+            for event in stream:
+                item = event.item
+                if event.type == "response.output_item.done":
+                    self.response.append(event.item)
+
+                if event.type == "response.output_item.done" and item.type == "function_call":
+                    func_name = item.name
+                    func_args = json.loads(item.arguments)
+
+                    result = 32 # call function and get result
+                    self.response.append({
+                        "type": "function_call_output",
+                        "call_id": item.call_id,
+                        "output": str(result)
+                    })
+
+                    got_func = True
+
+                yield event
+            
+            if not got_func:
+                break
 
 
 backends = {
